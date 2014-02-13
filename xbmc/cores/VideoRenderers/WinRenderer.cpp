06272d4f99360e1f229a3fe1cbd6095d9cb1bc21
Anton Fedchin <afedchin@ruswizards.com>
2014-02-13 15:41:46 +0400

    Add support more formats to DXVA/DXVA-HD renderers.

--- xbmc/cores/VideoRenderers/WinRenderer.cpp
+++ xbmc/cores/VideoRenderers/WinRenderer.cpp
@@ -394,6 +394,15 @@ unsigned int CWinRenderer::PreInit()
 
   m_formats.clear();
   m_formats.push_back(RENDER_FMT_YUV420P);
+  if (g_Windowing.IsTextureFormatOk(D3DFMT_L16, 0))
+  {
+    m_formats.push_back(RENDER_FMT_YUV420P10);
+    m_formats.push_back(RENDER_FMT_YUV420P16);
+  }
+  m_formats.push_back(RENDER_FMT_NV12);
+  m_formats.push_back(RENDER_FMT_YUYV422);
+  m_formats.push_back(RENDER_FMT_UYVY422);
+
 
   m_iRequestedMethod = CSettings::Get().GetInt("videoplayer.rendermethod");
 
@@ -421,15 +430,6 @@ unsigned int CWinRenderer::PreInit()
       return 0;
   }
   
-  if (g_Windowing.IsTextureFormatOk(D3DFMT_L16, 0))
-  {
-    m_formats.push_back(RENDER_FMT_YUV420P10);
-    m_formats.push_back(RENDER_FMT_YUV420P16);
-  }
-  m_formats.push_back(RENDER_FMT_NV12);
-  m_formats.push_back(RENDER_FMT_YUYV422);
-  m_formats.push_back(RENDER_FMT_UYVY422);
-
   return 0;
 }
 
